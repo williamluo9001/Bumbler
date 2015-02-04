1c0f220793b4a9f7b0a66d90594fd6b4e7ace05d
Master <subtle3ntropy@gmail.com>
2015-02-04 11:31:07 -0500

    welcome page

--- bumbler/db/migrate/20150203194831_create_comments.rb
+++ bumbler/db/migrate/20150203194831_create_comments.rb
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
-class CreateComments < ActiveRecord::Migration\
-	belongs_to User
-=======
 class CreateComments < ActiveRecord::Migration
-	belongs_to Users
->>>>>>> 27236936bfd87839ef54cb30253475fe8cfc88c9
+
+class CreateComments < ActiveRecord::Migration
+
   def change
     create_table :comments do |t|
       t.text :body
-
+      t.string :user_id
       t.timestamps null: false
     end
   end
