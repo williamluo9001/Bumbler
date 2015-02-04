386cfe325dea983aa7593787222fc2783f4bce5f
Master <subtle3ntropy@gmail.com>
2015-02-04 16:51:10 -0500

    Sign in fix

--- bumbler/app/controllers/sessions_controller.rb
+++ bumbler/app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController 
 
 	def create
-		@user = User.find_by(email: params [:email])
+		@user = User.find_by(email: params[:email])
 		if @user and @user.password == params[:password]
 			flash[:notice] = "Successfully signed into Bumbler!"
 			session[:user_id] = @user.user_id
@@ -12,8 +12,9 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-		session[:user_id] = nil
+		sessions[:user_id] = nil
 		flash[:notice] = "Bumble again soon!"
+		redirect_to root_path
 	end
 
 end
--- bumbler/config/routes.rb
+++ bumbler/config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
 
  resources :users
- resources :posts
+ resources :posts 
  resources :comments
 
  get 'signin', to: 'sessions#new'
- post '/signin', to: 'session#create'
+ post '/signin', to: 'sessions#create'
  delete '/logout', to: 'sessions#destroy'
 
 
-  
   root 'welcome#index'
 
   
